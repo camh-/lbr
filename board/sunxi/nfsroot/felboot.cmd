57c13a8c423e7c85533d8217e87104ac2e9d1c98
Cam Hutchison <camh@xdna.net>
2017-09-19 19:39:05 +1000

    nfsroot: Enable nfsv3 for nfsroot.
    Debian stretch no longer supports nfsv2 by default so use nfsv3.

--- board/sunxi/nfsroot/felboot.cmd
+++ board/sunxi/nfsroot/felboot.cmd
@@ -1,4 +1,4 @@
-setenv bootargs "earlyprintk console=ttyS0,115200 rootwait init=/sbin/init ip=dhcp root=/dev/nfs nfsroot=10.100.110.1:/srv/nfs/root/sunxi"
+setenv bootargs "earlyprintk console=ttyS0,115200 rootwait init=/sbin/init ip=dhcp root=/dev/nfs nfsroot=10.100.110.1:/srv/nfs/root/sunxi,nfsvers=3"
 setenv kernel_addr_r 0x42000000
 setenv fdt_addr_r 0x43000000
 setenv ramdisk_addr_r -
